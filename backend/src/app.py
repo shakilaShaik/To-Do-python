9a35b409babf3c19275fc8f7d9c1fd2627df1515
Shakila shaik <skshakilask67@gmail.com>
2025-05-13 17:35:01 +0530

    configured db with server

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,9 +1,16 @@
 from flask import Flask  
+from config.connection import db
+
+
 app = Flask(__name__)
+   
+app.db= db
+
 @app.route('/')
 def home():
    return 'hello world'
 if __name__ =='__main__':
+   
    app.run(debug=True)
 
 
---
+++ backend/src/config/connection.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Correctly get the URI from env
+MONGO_URI = os.getenv("MONGO_URI")
+
+# Connect to MongoDB Atlas
+client = MongoClient(MONGO_URI)
+
+# Reference to the database
+db = client["todos"]
+
+# Optional: test connection
+try:
+    client.admin.command('ping')
+    print("✅ Connected to MongoDB Atlas")
+except Exception as e:
+    print("❌ Connection failed:", e)
